87e823df51319ac556bf1c0a2296430dbe3e075c
Mika Tuupola <tuupola@appelsiini.net>
2023-03-30 15:46:50 +0700

    Refactor to explicit config

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,57 +11,92 @@ provider "linode" {
   token = var.linode_token
 }
 
-resource "linode_instance" "managers" {
-  count = 1
-  image  = "linode/rocky9"
-  label  = "swarm-manager-${format("%02s", count.index)}"
-  group  = "example"
-  region = "ap-south"
-  # region = "eu-central"
-  type            = "g6-standard-1"
-  authorized_keys = var.authorized_keys
-  root_pass       = var.root_pass
+locals {
+  node_settings = {
+    "manager-01" = { role = "manager", region = "ap-south", type = "g6-standard-4", vlan_ipam_address = "10.0.0.1/24" },
+    "manager-02" = { role = "manager", region = "ap-south", type = "g6-standard-4", vlan_ipam_address = "10.0.0.2/24" },
+    "manager-03" = { role = "manager", region = "ap-south", type = "g6-standard-4", vlan_ipam_address = "10.0.0.3/24" }
 
-  interface {
-    purpose = "public"
-  }
-
-  interface {
-    purpose = "vlan"
-    label = "swarm-vlan"
-    ipam_address = "10.0.0.${1 + count.index}/24"
+    "worker-01" = { role = "worker", region = "ap-south", type = "g6-standard-4", vlan_ipam_address = "10.0.0.6/24" },
   }
 }
 
-resource "linode_instance" "workers" {
-  count = 2
-  image  = "linode/rocky9"
-  label  = "swarm-worker-${format("%02s", count.index)}"
+resource "linode_instance" "nodes" {
+
+  for_each = local.node_settings
+
+  label  = each.key
+  region = each.value.region
+  type   = each.value.type
+  tags   = [each.value.role]
   group  = "example"
-  region = "ap-south"
-  # region = "eu-central"
-  type            = "g6-standard-1"
-  authorized_keys = var.authorized_keys
-  root_pass       = var.root_pass
+
+  # # region = "eu-central"
+  # #  curl https://api.linode.com/v4/linode/types | jq
+  # #type            = "g6-standard-1" # Linode 2 GB
+  # #type            = "g6-standard-2" # Linode 4 GB
+  # type            = "g6-standard-4" # Linode 8 GB
+}
+
+resource "linode_instance_config" "node_config" {
+  for_each = local.node_settings
+
+  linode_id = linode_instance.nodes[each.key].id
+  label     = "node-config"
 
   interface {
     purpose = "public"
   }
 
   interface {
-    purpose = "vlan"
-    label = "swarm-vlan"
-    ipam_address = "10.0.0.${10 + count.index}/24"
+    purpose      = "vlan"
+    label        = "swarm-vlan"
+    ipam_address = each.value.vlan_ipam_address
   }
+
+  devices {
+    sda { disk_id = linode_instance_disk.boot[each.key].id }
+    sdb { disk_id = linode_instance_disk.swap[each.key].id }
+    sdc { disk_id = linode_instance_disk.storage[each.key].id }
+  }
+
+  booted = true
+}
+
+resource "linode_instance_disk" "boot" {
+  for_each = local.node_settings
+
+  label     = "boot"
+  linode_id = linode_instance.nodes[each.key].id
+  size      = 20480
+
+  image           = "linode/rocky8"
+  root_pass       = var.root_pass
+  authorized_keys = var.authorized_keys
+}
+
+resource "linode_instance_disk" "swap" {
+  for_each = local.node_settings
+
+  label      = "swap"
+  linode_id  = linode_instance.nodes[each.key].id
+  size       = 256
+  filesystem = "swap"
+}
+
+resource "linode_instance_disk" "storage" {
+  for_each = local.node_settings
+
+  label      = "storage"
+  linode_id  = linode_instance.nodes[each.key].id
+  size       = linode_instance.nodes[each.key].specs.0.disk - 256 - 20480
+  filesystem = "raw"
 }
 
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/inventory.tftpl",
     {
-      manager_addresses = linode_instance.managers[*].ip_address
-      manager_labels = linode_instance.managers[*].label
-      worker_addresses = linode_instance.workers[*].ip_address
-      worker_labels = linode_instance.workers[*].label
+      nodes = linode_instance.nodes
     }
   )
   filename = "../ansible/inventory.ini"

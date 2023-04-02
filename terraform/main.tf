c75e59b65783ce6cc209c07d303f9c2bb7829322
Mika Tuupola <tuupola@appelsiini.net>
2023-04-02 12:12:38 +0700

    Add load balancer (#4)
    * Use 30GB root partition
    * Add http and https load balancers

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,11 @@ provider "linode" {
 
 locals {
   node_settings = {
-    "manager-01" = { role = "manager", region = "ap-south", type = "g6-standard-4", vlan_ipam_address = "10.0.0.1/24" },
-    "manager-02" = { role = "manager", region = "ap-south", type = "g6-standard-4", vlan_ipam_address = "10.0.0.2/24" },
-    "manager-03" = { role = "manager", region = "ap-south", type = "g6-standard-4", vlan_ipam_address = "10.0.0.3/24" }
+    "manager-01" = { role = "manager", region = "ap-south", type = "g6-standard-2", vlan_ipam_address = "10.0.0.1/24" },
+    "manager-02" = { role = "manager", region = "ap-south", type = "g6-standard-2", vlan_ipam_address = "10.0.0.2/24" },
+    "manager-03" = { role = "manager", region = "ap-south", type = "g6-standard-2", vlan_ipam_address = "10.0.0.3/24" }
 
-    "worker-01" = { role = "worker", region = "ap-south", type = "g6-standard-4", vlan_ipam_address = "10.0.0.6/24" },
+    #"worker-01" = { role = "worker", region = "ap-south", type = "g6-standard-2", vlan_ipam_address = "10.0.0.6/24" },
   }
 }
 
@@ -30,7 +30,7 @@ resource "linode_instance" "nodes" {
   type   = each.value.type
   tags   = [each.value.role]
   group  = "example"
-
+  private_ip = true
   # # region = "eu-central"
   # #  curl https://api.linode.com/v4/linode/types | jq
   # #type            = "g6-standard-1" # Linode 2 GB
@@ -68,7 +68,7 @@ resource "linode_instance_disk" "boot" {
 
   label     = "boot"
   linode_id = linode_instance.nodes[each.key].id
-  size      = 20480
+  size      = 30720
 
   image           = "linode/rocky8"
   root_pass       = var.root_pass
@@ -89,8 +89,55 @@ resource "linode_instance_disk" "storage" {
 
   label      = "storage"
   linode_id  = linode_instance.nodes[each.key].id
-  size       = linode_instance.nodes[each.key].specs.0.disk - 256 - 20480
+  size       = linode_instance.nodes[each.key].specs.0.disk - 256 - 30720
   filesystem = "raw"
+}
+
+
+resource "linode_nodebalancer" "swarm_lb" {
+    label = "swarm-lb"
+    region = "ap-south"
+    #client_conn_throttle = 60
+}
+
+resource "linode_nodebalancer_config" "swarm_lb_http_config" {
+    nodebalancer_id = linode_nodebalancer.swarm_lb.id
+    port = 80
+    protocol = "tcp"
+    check = "connection"
+    check_attempts = 3
+    check_timeout = 30
+    algorithm = "roundrobin"
+}
+
+resource "linode_nodebalancer_config" "swarm_lb_https_config" {
+    nodebalancer_id = linode_nodebalancer.swarm_lb.id
+    port = 443
+    protocol = "tcp"
+    check = "connection"
+    check_attempts = 3
+    check_timeout = 30
+    algorithm = "roundrobin"
+}
+
+resource "linode_nodebalancer_node" "swarm_lb_http_node" {
+    for_each = local.node_settings
+
+    nodebalancer_id = linode_nodebalancer.swarm_lb.id
+    config_id = linode_nodebalancer_config.swarm_lb_http_config.id
+    address = "${linode_instance.nodes[each.key].private_ip_address}:80"
+    label = "swarm-lb-http-node"
+    #weight = 50
+}
+
+resource "linode_nodebalancer_node" "swarm_lb_https_node" {
+    for_each = local.node_settings
+
+    nodebalancer_id = linode_nodebalancer.swarm_lb.id
+    config_id = linode_nodebalancer_config.swarm_lb_https_config.id
+    address = "${linode_instance.nodes[each.key].private_ip_address}:443"
+    label = "swarm-lb-https-node"
+    #weight = 50
 }
 
 resource "local_file" "ansible_inventory" {
